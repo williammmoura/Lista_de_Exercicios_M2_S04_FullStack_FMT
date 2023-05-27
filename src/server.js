b3c1e368b0ed39bf86ab50ee425b8893e542c5b9
William Monte de Moura <williamonte@gmail.com>
2023-05-27 18:19:37 -0300

    Exercício 01

---
+++ src/server.js
@@ -0,0 +1,8 @@
+const express = require("express")
+const server = express()
+
+
+
+// Iniciando o servidor
+server.listen(3333)
+console.log("O servidor está rodando na porta 3333: http://localhost:3333") 
